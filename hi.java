a0ed925e0fb7fe922bb9482e9e42b0eb25bceda8
G-Soma-Shekar <gadigesomashekargss@gmail.com>
2024-05-16 14:49:28 +0530

    First Sample Project Repository

--- hi.java
+++ hi.java
@@ -2,6 +2,6 @@ class hi
 {
     public static void main(String args[])
     {
-        System.out.println("Hi, I am from Java File")
+        System.out.println("Hi, I am from Java File");
     }
 }
